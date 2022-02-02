b336abba1ebc25a1b22bca0383b90a7350be499e
BarkeH <barker.hayden123@gmail.com>
2022-02-03 09:19:41 +1100

    Thursday morning: Added scissors paper rock

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
 
             Console.WriteLine("Welcome To Game Suite");
             string state = "Menu";
@@ -27,7 +28,7 @@ namespace ConsoleApp1
                 {"1", "Naughts and Crosses"},
                 {"2", "Scissors Paper Rock" },
                 {"3", "Exit" } };
-            string option = GetInput(gameOptions, "Please choose a game to play");
+            string option = GetInput(gameOptions, "Please choose a game to play").Item1;
             return option;
         }
         static string NaughtsAndCrosses()
@@ -36,19 +37,53 @@ namespace ConsoleApp1
               new Dictionary<string, string>(){
                 {"1", "1 Player"},
                 {"2", "2 Players" } };
-            string numPlayers = GetInput(PlayersDict,"Please choose how many players want to play");
+            string numPlayers = GetInput(PlayersDict,"Please choose how many players want to play").Item1;
             return "Menu";
         }
         static string ScissorsPaperRock()
         {
-            //todo
+            string play = "Yes";
+            int outcome = 1;
+
+            Dictionary<int, string> winner =
+              new Dictionary<int, string>(){
+                {1, "You win"},
+                {2, "You tie" },
+                {3, "You lose" } };
+            Random rnd = new Random();
+            Dictionary<string, string> PlayersChoice =
+              new Dictionary<string, string>(){
+                {"1", "Scissors"},
+                {"2", "Paper" },
+                {"3","Rock" } };
+            Dictionary<string, string> KeepPlaying =
+              new Dictionary<string, string>(){
+                {"1", "Yes"},
+                {"2", "No" }};
+            do
+            {
+                int choice = Int16.Parse(GetInput(PlayersChoice, "Please choose which choice you wish to use").Item2);
+                int robotChoice = rnd.Next(1, 3);
+                if (choice == robotChoice) { outcome = 2; }
+                else if (choice + 1 == robotChoice || choice - 2 == robotChoice) { outcome = 1; }
+                else if (choice == robotChoice + 1 || choice == robotChoice - 2) { outcome = 3; }
+                Console.WriteLine("You Chose: " + PlayersChoice[choice.ToString()]);
+                Console.WriteLine("Your Opponent Chose: " + PlayersChoice[robotChoice.ToString()]);
+
+                Console.WriteLine(winner[outcome]);
+                Console.WriteLine();
+                play = GetInput(KeepPlaying, "Would you like to keep playing?").Item1;
+            }
+            while (play == "Yes");
             return "Menu";
+
         }
-        static string GetInput(Dictionary<string, string> Options, string initialWrite)
+        static (string,string) GetInput(Dictionary<string, string> Options, string initialWrite)
         {
             string option;
             bool valid = false;
             bool once = false;
+            string input = "";
             do
             {
                 if (once == true)
@@ -62,7 +97,7 @@ namespace ConsoleApp1
                     Console.WriteLine(entry.Key + ": " + entry.Value);
                 }
                 Console.WriteLine("Type the number for the option you want to choose:");
-                string input = Console.ReadLine();
+                input = Console.ReadLine();
                 valid = Options.TryGetValue(input, out option);
                 once = true;
 
@@ -70,7 +105,8 @@ namespace ConsoleApp1
 
             }
             while (valid == false);
-            return option;
+            Console.WriteLine(option);
+            return (option, input);
         }
     }
 }
